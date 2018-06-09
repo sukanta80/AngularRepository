07f3cd96ac738d863a86bcb924118c582ddefa3e
sukanta80 <sukanta.misra@gmail.com>
2018-06-09 16:43:40 +0530

    Code for Chap 8 and 9 are here

--- APM-Start/src/app/product/product-list.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
- @Component({
-     selector: 'pm-products',
-     templateUrl: '/product-list.componenet.html'
- })
- export class ProductListComponent{
-
- }
